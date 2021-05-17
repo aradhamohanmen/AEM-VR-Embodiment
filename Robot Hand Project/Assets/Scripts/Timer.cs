5b1bf91bb73c5a18f4efdab2c6599a9202027d88
Orneas <46200633+Orneas@users.noreply.github.com>
2021-05-17 23:43:55 +0200

    Timer

--- Robot Hand Project/Assets/Scripts/Timer.cs
+++ Robot Hand Project/Assets/Scripts/Timer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,19 +6,40 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    bool timerActive = false;
     float currentTime;
-    public int startMinutes;
     public Text currenTimeText;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentTime = startMinutes * 60;
+        currentTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        currenTimeText.text = currentTime.ToString();
+        if (timerActive == true)
+        {
+            currentTime = currentTime + Time.deltaTime;
+        }
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        currenTimeText.text = time.ToString(@"mm\:ss\:fff");
+        }
+
+    public void StartTimer()
+    {
+        timerActive = true;
+    }
+
+    public void StopTimer()
+    {
+        SaveTime();
+        timerActive = false;
+    }
+
+    void SaveTime()
+    {
+        PlayerPrefs.SetString("Time", currenTimeText.text);
     }
 }
