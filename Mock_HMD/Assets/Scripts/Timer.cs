bed4946321db6955f7eba38af5fa7349cc041232
Orneas <46200633+Orneas@users.noreply.github.com>
2021-05-18 08:19:00 +0200

    Timer 1 done

--- Mock_HMD/Assets/Scripts/Timer.cs
+++ Mock_HMD/Assets/Scripts/Timer.cs
@@ -1,18 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    bool timerActive = false;
+    float currentTime;
+    public Text currenTimeText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timerActive == true)
+        {
+            currentTime = currentTime + Time.deltaTime;
+        }
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        currenTimeText.text = time.ToString(@"mm\:ss\:fff");
+    }
+    public void FlipTimer()
+    {
+        if (timerActive == true)
+        {
+            timerActive = false;
+        }
+        else if (timerActive == false)
+        {
+            timerActive = true;
+        }
+
+    }
+
+
+
+    public void StartTimer()
+    {
+        timerActive = true;
+    }
+
+    public void StopTimer()
+    {
+        SaveTime();
+        timerActive = false;
+    }
+
+    void SaveTime()
+    {
+        PlayerPrefs.SetString("Time", currenTimeText.text);
     }
 }
+
