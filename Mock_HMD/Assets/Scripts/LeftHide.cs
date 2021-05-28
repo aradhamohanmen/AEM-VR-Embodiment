9e11da8502bf06e425989ae5fe78b66a15224c68
aradhamohanmen <83578349+aradhamohanmen@users.noreply.github.com>
2021-05-28 17:56:23 +0200

    Final

---
+++ Mock_HMD/Assets/Scripts/LeftHide.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class LeftHide : MonoBehaviour
+{
+    public void HideOBJ(GameObject obj)
+    {
+        obj.GetComponent<Renderer>().enabled = false;
+    }
+
+    public void position(GameObject obj)
+    {
+        Debug.Log("Left=" + obj.transform.position.ToString("F3"));
+            }
+    public void ShowOBJ(GameObject obj)
+    {
+        obj.GetComponent<Renderer>().enabled = true;
+    }
+
+
+}
---
+++ Mock_HMD/Assets/Scripts/RightHide.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class RightHide : MonoBehaviour
+{
+    public void HideOBJ(GameObject obj)
+    {
+        obj.GetComponent<Renderer>().enabled = false;
+    }
+
+    public void position(GameObject obj)
+    {
+        Debug.Log("Right =" + obj.transform.position.ToString("F3"));
+            }
+    public void ShowOBJ(GameObject obj)
+    {
+        obj.GetComponent<Renderer>().enabled = true;
+    }
+
+
+}
